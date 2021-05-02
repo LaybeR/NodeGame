9885d07b0a07bdadfdf44231d7f77966e500f0fd
LaybeR <42009151+LaybeR@users.noreply.github.com>
2021-05-02 15:05:16 +0200

    Update JSONReader.java

--- src/main/java/JSONControlling/JSONReader.java
+++ src/main/java/JSONControlling/JSONReader.java
@@ -1,33 +1,13 @@
 package JSONControlling;
 
-import Objects.EventEntry;
-import org.json.JSONArray;
 import org.json.JSONObject;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.LinkedList;
 
 public class JSONReader {
 
-    public void fillLinkedListWithEventEntries(JSONObject oWiki, LinkedList<EventEntry> eventEntries) {
-        JSONArray oEvents = oWiki.getJSONArray("Events");
-        for (int i = 0; i < oEvents.length(); i++) {
-            JSONObject oEvent = oEvents.getJSONObject(i);
-            EventEntry event = new EventEntry(oEvent);
-            if (event.doesEventDateExist())
-                eventEntries.add(event);
-        }
+    public JSONObject readGamesFile() {
+        JSONObject oGameFile = new JSONObject();
+
+        return oGameFile;
     }
 
-    public JSONObject getWikiAsJSONObject() {
-        JSONObject oWiki = null;
-        try {
-            String jsonString = new String(Files.readAllBytes(Paths.get("wiki.json")));
-            oWiki = new JSONObject(jsonString);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return oWiki;
-    }
 }
