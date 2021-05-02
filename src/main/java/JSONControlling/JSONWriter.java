8008057ffac553c90fd07277b879fcdbae9e9e29
LaybeR <42009151+LaybeR@users.noreply.github.com>
2021-05-02 15:05:30 +0200

    Update JSONWriter.java

--- src/main/java/JSONControlling/JSONWriter.java
+++ src/main/java/JSONControlling/JSONWriter.java
@@ -1,56 +1,5 @@
 package JSONControlling;
 
-import Objects.EventEntry;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import java.io.File;
-import java.io.FileWriter;
-import java.nio.file.Paths;
-import java.util.LinkedList;
-
 public class JSONWriter {
 
-    public JSONArray getJSONArrayOfEventEntries(LinkedList<EventEntry> eventEntries) {
-        JSONArray oEvents = new JSONArray();
-        for (EventEntry event : eventEntries)
-            oEvents.put(getEventEntryAsJSONObject(event));
-        return oEvents;
-    }
-
-    public void writeToWikiJSON(JSONObject oWiki) {
-        File wikiJSONFile = new File(String.valueOf(Paths.get("wiki.json")));
-        String oWikiAsString = oWiki.toString();
-        oWikiAsString = reformatWikiString(oWikiAsString);
-        writeToFile(wikiJSONFile, oWikiAsString);
-    }
-
-    private JSONObject getEventEntryAsJSONObject(EventEntry event) {
-        JSONObject oEvent = OrderedJSONObjectFactory.create();
-        oEvent.put("day", event.getDay());
-        oEvent.put("month", event.getMonth());
-        oEvent.put("year", event.getYear());
-        oEvent.put("title", event.getTitle());
-        oEvent.put("entry", event.getEntry());
-        return oEvent;
-    }
-
-    private String reformatWikiString(String oWikiString) {
-        oWikiString = oWikiString.replaceAll("\\[","[\n");
-        oWikiString = oWikiString.replaceAll("]","\n]");
-        oWikiString = oWikiString.replaceAll("\\{","{\n");
-        oWikiString = oWikiString.replaceAll("}","\n}");
-        oWikiString = oWikiString.replaceAll(",",",\n");
-        return oWikiString;
-        //oWikiString = oWikiString.replaceAll("","");
-    }
-
-    private void writeToFile(File f, String s) {
-        try {
-            FileWriter writer = new FileWriter(f);
-            writer.write(s);
-            writer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
