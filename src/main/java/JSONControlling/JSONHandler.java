2541fd618d65efa63a8c34cdd4d4d799459f0910
LaybeR <42009151+LaybeR@users.noreply.github.com>
2021-05-02 15:04:49 +0200

    Update JSONHandler.java

--- src/main/java/JSONControlling/JSONHandler.java
+++ src/main/java/JSONControlling/JSONHandler.java
@@ -1,36 +1,17 @@
 package JSONControlling;
 
-import Objects.EventEntry;
 import org.json.JSONObject;
-
-import java.util.LinkedList;
 
 public class JSONHandler {
 
     private boolean readJSON = false;
-    private JSONObject oWiki = null;
+    private JSONObject oGameFile = null;
     private final JSONReader jsonReader = new JSONReader();
     private final JSONWriter jsonWriter = new JSONWriter();
 
-    public void updateWikiEvents(LinkedList<EventEntry> eventEntries) {
-        oWiki.put("Events", jsonWriter.getJSONArrayOfEventEntries(eventEntries));
-    }
-
-    public void writeWikiToJSON() {
-        jsonWriter.writeToWikiJSON(oWiki);
-    }
-
-    public void readWikiJSON() {
+    public void readGameFilesJSON() {
         if (readJSON) return;
-        oWiki = jsonReader.getWikiAsJSONObject();
+        oGameFile = jsonReader.readGamesFile();
         readJSON = true;
-    }
-
-    public LinkedList<EventEntry> getEventEntriesFromWikiJSON() {
-        LinkedList<EventEntry> eventEntries = new LinkedList<>();
-        if (oWiki == null)
-            return eventEntries;
-        jsonReader.fillLinkedListWithEventEntries(oWiki, eventEntries);
-        return eventEntries;
     }
 }
